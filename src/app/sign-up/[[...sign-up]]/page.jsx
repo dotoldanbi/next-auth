48b59874245690b8ff10838cc0c5c1291f39d5cb
jhsong <jh@song>
2025-03-28 16:52:20 +0900

    Authentication

---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SignUp } from "@clerk/nextjs";
+export default function Page() {
+  return (
+    <>
+      <div className="flex items-center justify-center p-3">
+        <SignUp />
+      </div>
+    </>
+  );
+}
