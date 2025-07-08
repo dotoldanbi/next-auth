5106b67442c529cc69c4abd998db89844afd139d
jhsong <jh@song>
2025-07-08 16:59:12 +0900

    init next-auth with clerk

---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <div className="flex items-center justify-center p-3">
+        <SignUp />
+      </div>
+    </div>
+  );
+}
